a078776e33e0f560d99a784cee1fdbf456e11826
Felipe Anzini <71108@PROWAY.TREINA>
2021-08-09 20:58:21 -0300

    Subindo o Projeto

---
+++ src/Conta.java
@@ -0,0 +1,59 @@
+
+public class Conta {
+
+	private double saldo;
+	private String titular;
+	private int numero;
+	private String agencia;
+	
+	
+	
+	public Conta() {
+		
+	}
+
+	public Conta(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public void sacar(double valor) {
+		saldo = saldo - valor;		
+		
+	}
+	
+	public void depositar (double valor) {
+		saldo = saldo + valor;
+		
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	
+	
+}
---
+++ src/TestaConta.java
@@ -0,0 +1,18 @@
+
+public class TestaConta {
+	
+	public static void main(String[] args) {
+		
+		Conta c1 = new Conta(1200);
+		
+		c1.setTitular("Felipe");
+		c1.setAgencia("003");
+		c1.setNumero(12345);
+		c1.sacar(30);
+		c1.depositar(500);
+		
+		System.out.println(c1.getSaldo());
+		
+		
+	}
+}
