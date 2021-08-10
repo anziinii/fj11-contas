5e86f5295561169fd4b471cd103b719ea09e1fd5
Felipe <felipe_anzini@hotmail.com>
2021-08-09 21:53:56 -0300

    Herença

--- src/TestaConta.java
+++ src/TestaConta.java
@@ -9,9 +9,20 @@ public class TestaConta {
 		c1.setAgencia("003");
 		c1.setNumero(12345);
 		c1.sacar(30);
-		c1.depositar(500);
+		//c1.depositar(500);
 		
 		System.out.println(c1.getSaldo());
+		
+		
+		ContaCorrente cc = new ContaCorrente(1200);
+		cc.setTitular("Clientola");
+		cc.sacar(30);
+		System.out.println(cc.getSaldo());
+		
+		
+		//ContaPoupanca cp = new ContaPoupanca();
+		
+		
 		
 		
 	}
